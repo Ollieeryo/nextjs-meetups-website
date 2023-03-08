49cd9965d90fbdaefbebbb92671cb914366e3638
Ollieeryo <ollieeryo@gmail.com>
2023-03-08 23:12:02 +0800

    fix fallback

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -32,7 +32,7 @@ export async function getStaticPaths() {
     paths: meetups.map((meetup) => ({
       params: { meetupid: meetup._id.toString()},
     })),
-    fallback: false,
+    fallback: 'blocking',
   };
 }
 
